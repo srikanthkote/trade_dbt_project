9765feaee8b46facdc815e0a359425a52217129f
Srikanth Kotekar <tracert.social@gmail.com>
2025-04-14 19:12:49 +0200

    removed spotify models

--- models/intermediate/spotify/silver_spotify_playlists.sql
+++
@@ -1,8 +0,0 @@
-{{
-    config(
-        materialized = 'view'
-    )
-}}
-
-SELECT * Exclude('tracks'), now() AS ingest_timestamp
-    FROM {{ ref('bronze_spotify_playlists') }}
--- models/staging/spotify/bronze_spotify_playlists.sql
+++
@@ -1,8 +0,0 @@
-{{
-    config(
-        materialized = 'table'
-    )
-}}
-
-SELECT * , now() AS ingest_timestamp
-    FROM read_parquet({{ source('external_source', 'bronze_spotify_playlists') }})
