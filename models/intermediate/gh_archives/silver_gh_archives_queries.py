12944083277dd637fb7354b992b49d37cc9b47fb
Srikanth Kotekar <tracert.social@gmail.com>
2025-05-24 18:04:26 +0200

    Update silver_gh_archives_queries.py

--- models/intermediate/gh_archives/silver_gh_archives_queries.py
+++ models/intermediate/gh_archives/silver_gh_archives_queries.py
@@ -5,27 +5,44 @@ from time import perf_counter_ns
 # model that runs many queries in order to check latencies
 def model(dbt, session):
 
+    # DataFrame representing an upstream model
+    silver_alldata_df = dbt.ref("silver_gh_archives_deltascan_polars").df()
+
     queries = [
-        "SUMMARIZE silver_gh_archives_deltascan_polars",
+        # query to run against a Python Dataframe representing the upstream model
+        # illustrates the seamless integration and querying capabilities of DuckDB inside the
+        # Python ecosystem; it does not matter whether the relation has been created as a table,
+        # sourced from a file, or represented by a pandas DataFrame.
+        "select actor_id, type, created_at, repo_id, repo_name from silver_alldata_df where actor_id = '49699333' and type = 'PullRequestEvent' and created_at between '2025-01-01 00:00:00.000' and '2025-01-01 01:00:00.000'",
+
+        # queries to run against materialized Duckdb tables
         "select count(1) from silver_gh_archives_deltascan_polars",
         "select actor_id, type, created_at, repo_id, repo_name from silver_gh_archives_deltascan_polars where actor_id = '49699333' and type = 'PullRequestEvent' and created_at between '2025-01-01 00:00:00.000' and '2025-01-01 01:00:00.000'",
         "select a.actor_id, a.type, a.created_at, a.repo_id, a.repo_name from silver_gh_archives_pullrequests a, silver_gh_archives_forks b	where a.actor_id = b.actor_id and a.actor_id = '163356328' and a.created_at between '2025-01-01 00:00:00.000' and '2025-01-02 01:00:00.000'",
+
+        # query that run against parquet files
         "select actor_id, type, created_at, repo_id, repo_name from '/Users/srikanthkotekar/Downloads/gh_archives/raw/*.parquet' where actor_id = '49699333' and created_at between '2025-01-01 00:00:00.000' and '2025-01-02 01:00:00.000'",
     ]
 
-    # DataFrame representing an upstream model
-    upstream_model = dbt.ref("silver_gh_archives_deltascan_polars").df()
     con = duckdb.connect("/Users/srikanthkotekar/code/analytics-workspace/trade-statistics.duckdb")
     con.sql("CREATE INDEX idx_actor_id ON silver_gh_archives_deltascan_polars(actor_id);")
     con.sql("CREATE INDEX idx_type ON silver_gh_archives_deltascan_polars(type);")
     con.sql("CREATE INDEX idx_created_at ON silver_gh_archives_deltascan_polars(created_at);")
+    con.sql("CREATE INDEX idx_repo_id ON silver_gh_archives_deltascan_polars(repo_id);")
+    con.sql("CREATE INDEX idx_repo_name ON silver_gh_archives_deltascan_polars(repo_name);")
+
     con.sql("CREATE INDEX idx_pr_actor_id ON silver_gh_archives_pullrequests(actor_id);")
     con.sql("CREATE INDEX idx_pr_type ON silver_gh_archives_pullrequests(type);")
     con.sql("CREATE INDEX idx_pr_created_at ON silver_gh_archives_pullrequests(created_at);")
+    con.sql("CREATE INDEX idx_pr_repo_id ON silver_gh_archives_pullrequests(repo_id);")
+    con.sql("CREATE INDEX idx_pr_repo_name ON silver_gh_archives_pullrequests(repo_name);")
+    
     con.sql("CREATE INDEX idx_fr_actor_id ON silver_gh_archives_forks(actor_id);")
     con.sql("CREATE INDEX idx_fr_type ON silver_gh_archives_forks(type);")
     con.sql("CREATE INDEX idx_fr_created_at ON silver_gh_archives_forks(created_at);")
-    
+    con.sql("CREATE INDEX idx_fr_repo_id ON silver_gh_archives_forks(repo_id);")
+    con.sql("CREATE INDEX idx_fr_repo_name ON silver_gh_archives_forks(repo_name);")
+            
     for query in queries:
         t1_start = perf_counter_ns()
 
@@ -36,18 +53,26 @@ def model(dbt, session):
         print(f"{query} - {elapsed}ms")
   
         # Print the result
-        print(result)
+        # print(result)
 
     # explicitly drop the indices to support regeneration
     con.sql("DROP INDEX idx_actor_id")
     con.sql("DROP INDEX idx_type")
     con.sql("DROP INDEX idx_created_at")
+    con.sql("DROP INDEX idx_repo_id")
+    con.sql("DROP INDEX idx_repo_name")
+
     con.sql("DROP INDEX idx_pr_actor_id")
     con.sql("DROP INDEX idx_pr_type")
     con.sql("DROP INDEX idx_pr_created_at")
+    con.sql("DROP INDEX idx_pr_repo_id")
+    con.sql("DROP INDEX idx_pr_repo_name")
+
     con.sql("DROP INDEX idx_fr_actor_id")
     con.sql("DROP INDEX idx_fr_type")
     con.sql("DROP INDEX idx_fr_created_at")
+    con.sql("DROP INDEX idx_fr_repo_id")
+    con.sql("DROP INDEX idx_fr_repo_name")
 
     # explicitly close the connection
     con.close()
