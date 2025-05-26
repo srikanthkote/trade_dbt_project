0b7f94793ddc205a6bc21870406047202c2dbf61
Srikanth Kotekar <tracert.social@gmail.com>
2025-05-26 10:12:35 +0200

    added scatter plot for time window
    added scatter plot for visualizing events per time window

--- infrastructure/streamlit/app.py
+++ infrastructure/streamlit/app.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import pandas as pd
 import duckdb
+import plotly.express as px
 
 # App title
 st.title("WELCOME TO STREAMLIT APPS")
 
 # Header
 st.subheader("silver_gh_archives_deltascan_polars")
-st.code("select actor_id, type, created_at, repo_id, repo_name from silver_gh_archives_deltascan_polars where actor_id = '49699333' and type = 'PullRequestEvent' and created_at between '2025-01-01 00:00:00.000' and '2025-01-01 01:00:00.000'")
+st.code(
+    "select actor_id, type, created_at, repo_id, repo_name from silver_gh_archives_deltascan_polars where actor_id = '49699333' and type = 'PullRequestEvent' and created_at between '2025-01-01 00:00:00.000' and '2025-01-01 01:00:00.000'"
+)
 
-con = duckdb.connect("/Users/srikanthkotekar/code/analytics-workspace/trade-statistics.duckdb", read_only=True)
+con = duckdb.connect(
+    "/Users/srikanthkotekar/code/analytics-workspace/trade-statistics.duckdb",
+    read_only=True,
+)
 
 query = f"""
     select actor_id, type, created_at, repo_id, repo_name from silver_gh_archives_deltascan_polars where actor_id = '49699333' and type = 'PullRequestEvent' and created_at between '2025-01-01 00:00:00.000' and '2025-01-01 01:00:00.000'"""
@@ -27,6 +33,21 @@ df = con.sql(query).df()
 st.dataframe(df)
 st.bar_chart(df, x="repo_name", y="counts", horizontal=True)
 
-st.scatter_chart(df, x="repo_id", y="counts",color=["#FF0000"])
+# Header
+st.subheader("silver_gh_archives_groupby_timewindow")
+st.code("SELECT * FROM silver_gh_archives_groupby_timewindow")
+query = f"""
+    SELECT * FROM silver_gh_archives_groupby_timewindow"""
+df = con.sql(query).df()
+st.dataframe(df)
+fig = px.scatter(
+    df,
+    x="created_at",
+    y="len",
+    color="type",
+    color_discrete_sequence=px.colors.qualitative.Plotly,
+)
+st.plotly_chart(fig, use_container_width=True)
 
+## closing the DB connection
 con.close()
